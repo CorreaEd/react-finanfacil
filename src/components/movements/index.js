d32aac0acae4ccc42d44ab001a4e7de41d663304
correaed <correaeduardo96@outlook.com>
2023-11-16 17:26:54 -0300

    Create movements

---
+++ src/components/movements/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    StyleSheet
+} from 'react-native';
+
+export default function movements() {
+ return (
+   <View style={styles.container}>
+        <Text>TESTE 123</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container:{
+
+    }
+})
